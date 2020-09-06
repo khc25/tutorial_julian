fd669616bb5c22c40e3b9969c614903c253698f8
Brian Chow <32953270+khc25@users.noreply.github.com>
2020-09-06 20:09:36 +0800

    updata

--- day_2/test.js
+++ day_2/test.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+
+//Method 1
+// function ButtonClick () {
+//     let inputValue = document.getElementById("one").value;
+//     console.log(inputValue)
+
+//     document.getElementById('three').innerHTML = inputValue;
+// }
+//Method 2
+document.getElementById('two').addEventListener('click',()=> {
+    let inputValue = document.getElementById("one").value;
+    console.log(inputValue)
+    document.getElementById('three').innerHTML = inputValue;
+})
